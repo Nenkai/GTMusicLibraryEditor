fc1bfe49cde06eaec224bd0c37c225e453e49f6a
Nenkai <neenkaai@gmail.com>
2021-03-04 17:05:32 +0100

    Refactors

--- GTMusicInfEditor/App.xaml.cs
+++ GTMusicInfEditor/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace GTBGMLibraryEditor
+namespace GTMusicInfEditor
 {
     /// <summary>
     /// Interaction logic for App.xaml
